29350dde1b2b1749408333fa92257ccb240ddf9a
jdiaz-he <jdiaz-he@student.42madrid.com>
2023-02-11 09:34:59 +0100

    updated memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jdiaz-he <jdiaz-he@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:03:42 by jdiaz-he          #+#    #+#             */
-/*   Updated: 2023/02/10 12:40:31 by jdiaz-he         ###   ########.fr       */
+/*   Updated: 2023/02/11 09:26:38 by jdiaz-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	count;
 
 	count = n;
-	if (dest || src)
+	if (!dest && !src)
+		return (0);
+	while (count > 0)
 	{
-		while (count > 0)
-		{
-			*(unsigned char *) dest = *(const unsigned char *) src;
-			dest++;
-			src++;
-			count--;
-		}
+		*(unsigned char *) dest = *(const unsigned char *) src;
+		dest++;
+		src++;
+		count--;
 	}
 	return (dest - n);
 }
